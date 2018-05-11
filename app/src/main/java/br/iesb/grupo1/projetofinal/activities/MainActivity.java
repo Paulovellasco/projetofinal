09da2dde6b1278cd0e32def137bfdda858330c53
Paulo <phsvellasco@gmail.com>
2018-05-11 20:45:17 -0300

    temp main activity functions

--- app/src/main/java/br/iesb/grupo1/projetofinal/activities/MainActivity.java
+++ app/src/main/java/br/iesb/grupo1/projetofinal/activities/MainActivity.java
@@ -1,15 +1,45 @@
 package br.iesb.grupo1.projetofinal.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.iesb.grupo1.projetofinal.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnGoToEditProfile;
+    Button btnLogOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGoToEditProfile = findViewById(R.id.btnGoToEditProfile);
+        btnLogOut = findViewById(R.id.btnLogOut);
+
+        btnGoToEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent t = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(t);
+            }
+        });
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.signOut();
+
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
