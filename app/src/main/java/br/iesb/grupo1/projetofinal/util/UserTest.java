fbaf9f6d3a40062097c5cbb646a84da69a13df1b
Paulo <phsvellasco@gmail.com>
2018-05-17 20:52:26 -0300

    userTest class

---
+++ app/src/main/java/br/iesb/grupo1/projetofinal/util/UserTest.java
@@ -0,0 +1,27 @@
+package br.iesb.grupo1.projetofinal.util;
+
+/**
+ * Created by Paulo on 17/05/2018.
+ */
+
+public class UserTest {
+
+    String name;
+    String alias;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
