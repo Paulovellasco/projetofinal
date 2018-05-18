2e4b9ac57f6d6c7bd549ffb6e30ed0ea6a8a5d75
MTakashiMori <Df075704>
2018-05-18 20:53:57 -0300

    Recycle View Funcionando

--- app/src/main/java/br/iesb/grupo1/projetofinal/activities/ListActivity.java
+++ app/src/main/java/br/iesb/grupo1/projetofinal/activities/ListActivity.java
@@ -1,7 +1,16 @@
 package br.iesb.grupo1.projetofinal.activities;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -23,7 +32,8 @@ public class ListActivity extends AppCompatActivity {
     DatabaseReference dr;
     UserTest user;
     ArrayList <UserTest> userArray = new ArrayList<>();
-
+    UserRecycleViewAdapter rva;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +48,11 @@ public class ListActivity extends AppCompatActivity {
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     UserTest usuario = ds.getValue(UserTest.class);
                     userArray.add(usuario);
+
+
                 }
+
+                rva.notifyDataSetChanged();
             }
 
             @Override
@@ -47,13 +61,69 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
+        rva = new UserRecycleViewAdapter(this,userArray);
+        rv = findViewById(R.id.testRecycle);
+        rv.setAdapter(rva);
+        //rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+
+
 
     }
 
+    class UserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        public TextView txtRecycleName;
+        public TextView txtRecycleAlias;
+        public UserTest user;
 
-    private void getData(){
+        public UserViewHolder(View itemView) {
+            super(itemView);
 
+            this.txtRecycleName = itemView.findViewById(R.id.txtRecycleName);
+            this.txtRecycleAlias = itemView.findViewById(R.id.txtRecycleAlias);
+            itemView.setOnClickListener(this);
 
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) itemView.getLayoutParams();
+            params.setMargins(35, 0, 0, 0);
+            itemView.setLayoutParams(params);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            //TODO no clique o card do user faz algo
+        }
+    }
+
+    class UserRecycleViewAdapter extends RecyclerView.Adapter<UserViewHolder> {
+        private Context contexto;
+        private List<UserTest> listaUsuarios;
+
+        public UserRecycleViewAdapter(Context contexto, List<UserTest> listaUsuarios) {
+            this.contexto = contexto;
+            this.listaUsuarios = listaUsuarios;
+        }
+
+        @Override
+        public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(contexto).inflate(R.layout.user, parent, false);
+            return new UserViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+            UserTest user = listaUsuarios.get(position);
+
+            holder.txtRecycleName.setText(user.getName());
+            holder.txtRecycleAlias.setText(user.getAlias());
+            holder.user = user;
+        }
+
+        @Override
+        public int getItemCount() {
+            return listaUsuarios.size();
+        }
     }
 }
--- app/src/main/java/br/iesb/grupo1/projetofinal/util/UserRecycleViewAdapter.java
+++ app/src/main/java/br/iesb/grupo1/projetofinal/util/UserRecycleViewAdapter.java
@@ -15,32 +15,3 @@ import br.iesb.grupo1.projetofinal.R;
  * Created by 1614290072 on 17/05/2018.
  */
 
-public class UserRecycleViewAdapter extends RecyclerView.Adapter<UserViewHolder> {
-    private Context contexto;
-    private List<UserTest> listaUsuarios;
-
-    public UserRecycleViewAdapter(Context contexto, List<UserTest> listaUsuarios) {
-        this.contexto = contexto;
-        this.listaUsuarios = listaUsuarios;
-    }
-
-    @Override
-    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(contexto).inflate(R.layout.user,parent,false);
-        return new UserViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
-        UserTest user = listaUsuarios.get(position);
-
-        holder.txtRecycleName.setText(user.getName());
-        holder.txtRecycleAlias.setText(user.getAlias());
-        holder.user = user;
-    }
-
-    @Override
-    public int getItemCount() {
-        return listaUsuarios.size();
-    }
-}
