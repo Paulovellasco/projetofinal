a04a1c51dec9921afbb6f68ee394ba92ebb71277
Paulo <phsvellasco@gmail.com>
2018-05-28 16:45:11 -0300

    Accessing TCU API (exception on JobListActivity)

--- app/src/main/java/br/iesb/grupo1/projetofinal/activities/JobListActivity.java
+++ app/src/main/java/br/iesb/grupo1/projetofinal/activities/JobListActivity.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
 
 import br.iesb.grupo1.projetofinal.R;
 import br.iesb.grupo1.projetofinal.util.Job;
+import br.iesb.grupo1.projetofinal.util.JobStation;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.http.GET;
 
 public class JobListActivity extends AppCompatActivity {
 
-    Job.JobService jService;
-    String API_BASE_URL = "https://api.catho.com.br/vagas/v1/";
+    //INTERFACE
+    public interface JobStationService{
+        @GET("/rest/emprego")
+        Call<List<JobStation>> listarPostos();
+    }
+
+    JobStationService jService;
+    String API_BASE_URL = "http://mobile-aceite.tcu.gov.br/mapa-da-saude/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +38,26 @@ public class JobListActivity extends AppCompatActivity {
                 .build();
 
 
-        jService = retrofitJob.create(Job.JobService.class);
+        jService = retrofitJob.create(JobStationService.class);
+        Call<List<JobStation>> requisition = jService.listarPostos();
+        requisition.enqueue(new Callback<List<JobStation>>() {
+            @Override
+            public void onResponse(Call<List<JobStation>> call, Response<List<JobStation>> response) {
+                if(response.isSuccessful()){
+                    List<JobStation> list = response.body();
 
+                    if(list != null && list.size() != 0){
+                        JobStation j = list.get(0);
+                        Log.d("A", "<<<<<<<<<<<<<<<<<< Posto n: " + j.codPosto + " >>>>>>>>>>>>>>>>>>>>>>>>>");
+                    }
+                }
+            }
 
+            @Override
+            public void onFailure(Call<List<JobStation>> call, Throwable t) {
+                Toast.makeText(JobListActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
---
+++ app/src/main/java/br/iesb/grupo1/projetofinal/util/JobStation.java
@@ -0,0 +1,115 @@
+package br.iesb.grupo1.projetofinal.util;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Paulo on 28/05/2018.
+ */
+
+public class JobStation {
+
+    public String codPosto;
+    public String nome;
+    public String entidadeConveniada;
+    public String endereco;
+    public String bairro;
+    public String cep;
+    public String telefone;
+    public String municipio;
+    public String uf;
+    public double lat;
+    public double longitude;
+
+    public String getCodPosto() {
+        return codPosto;
+    }
+
+    public void setCodPosto(String codPosto) {
+        this.codPosto = codPosto;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEntidadeConveniada() {
+        return entidadeConveniada;
+    }
+
+    public void setEntidadeConveniada(String entidadeConveniada) {
+        this.entidadeConveniada = entidadeConveniada;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
